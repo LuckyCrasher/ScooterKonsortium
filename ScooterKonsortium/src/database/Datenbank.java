d961458f5f7b20a3dba2259d7a1af8691a40579d
root <root@archlinux>
2022-06-04 04:26:54 +0200

    Added Database Files

--- ScooterKonsortium/src/database/Datenbank.java
+++ ScooterKonsortium/src/database/Datenbank.java
@@ -1,5 +1,7 @@
 package database;
 
+import java.sql.SQLException;
+
 import konsortiumdata.*;
 
 public class Datenbank {
@@ -7,18 +9,72 @@ public class Datenbank {
 	
 	
 	public void load() {
-		
+		oDBConnection.connect();
 	}
 	
-	public void putScooter(Scooter poScooter) {
+	public void putScooter(Scooter poScooter) throws SQLException {
+		int koordX = poScooter.getx();
+		int koordY = poScooter.gety();
+		int ladezustand = poScooter.getCurrentProzent();
+		int strecke = poScooter.getCoveredKm();
+		boolean fahrtzustand = poScooter.getCurrentStatus();
+		double einnahmen = poScooter.getCurrentEarn();
+		String name = poScooter.getFirmaOwning();
+		int firmenID = getFirmaID(name);
 		
+		String sQuerry = "INSERT INTO firma_daten (aufenthalt_x, aufenthalt_y, ladezustand, zurueckgelegte_strecke, fahrtzustand_aktuell,"
+				+ " einnahmen_aktuell, firmenID) VALUES (" + koordX + ", " + koordY + ", " + ladezustand + ", " + strecke + ", " + fahrtzustand + ", " + einnahmen + ", " + firmenID + ");";
+		
+		oDBConnection.sendQuerryNoReturn(sQuerry);
 	}
 	
-	public void putFirma(Firma poFirma) {
-		
+	public int getFirmaID(String name) throws SQLException {
+		String sQuerry = "SELECT ID FROM firma_daten WHERE name = " + name + ";";
+		String[] returnValue = oDBConnection.sendQuerry(sQuerry);
+		String returnValue2 = returnValue[0];
+		int i = Integer.parseInt(returnValue2);
+		return i;
 	}
 	
-	public void putLadepunkt(Ladepunkt poLadepunkt) {
+	public void putFirma(Firma poFirma) throws SQLException {
+		String name = poFirma.getName();
+		String adresse = poFirma.getAdresse();
+		String hotline = poFirma.getHotline();
 		
+		int PLZ = poFirma.getPLZ();
+		String stadt = poFirma.getStadt();
+		String sQuerry = "INSERT IGNORE INTO ort (stadt, PLZ) VALUES (\"" + stadt + "\", \"" + PLZ + "\");";
+		putStadt(sQuerry);
+		
+		int ortsID = getOrt(PLZ);
+		
+		String sQuerry2 = "INSERT INTO firma_daten (name, adresse, hotline, ortsID) VALUES (\"" + name + "\", \"" + adresse + "\", \"" + hotline + "\", " + ortsID + ");";
+		oDBConnection.sendQuerryNoReturn(sQuerry2);
+	}
+	
+	public void putStadt(String sQuerry) throws SQLException {
+		oDBConnection.sendQuerryNoReturn(sQuerry);
+	}
+	
+	public int getOrt(int PLZ) throws SQLException {
+		String sQuerry = "SELECT ID FROM ort WHERE PLZ = " + PLZ + ";";
+		String[] returnValue = oDBConnection.sendQuerry(sQuerry);
+		String returnValue2 = returnValue[0];
+		int i = Integer.parseInt(returnValue2);
+		return i;
+	}
+	
+	public void putLadepunkt(Ladepunkt poLadepunkt) throws SQLException {
+		String name = poLadepunkt.getNameLadepunkt();
+		int max_kapazitaet = poLadepunkt.getLadeCap();
+		int aktuell_benutzt = poLadepunkt.getCurrentUse();
+		int fixe_koords_x = poLadepunkt.getx();
+		int fixe_koords_y = poLadepunkt.gety();
+		
+		String firma = poLadepunkt.getFirmaOwning();
+		int firmenID = getFirmaID(name);
+		
+		String sQuerry = "INSERT INTO ladepunkt_daten (name, max_kapazitaet, aktuell_benutzt, fixe_koords_x, fixe_koords_y, firmenID) VALUES (\"" + name + "\", " + max_kapazitaet + ", " + aktuell_benutzt + ", " + fixe_koords_x + ", " + fixe_koords_y + ", " + firmenID + ");";
+		oDBConnection.sendQuerryNoReturn(sQuerry);
 	}
 }
--- ScooterKonsortium/src/database/DatenbankVerbindung.java
+++ ScooterKonsortium/src/database/DatenbankVerbindung.java
@@ -1,21 +1,55 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatenbankVerbindung {
 	//private oConnection;
 	private String sAddress;
 	private int iPort;
-	
+	Connection conn;
 	//ha siehe Diagramm
 	/*public void connect(mysqlConnection) {
 		
 	}*/
+	String url = "jdbc:mysql://192.168.178.21:3306/ik_scooter";
+	String user = "nils312";
+	String password = "admin";
 	
+	public void connect() {
+		try  {
+			conn = DriverManager.getConnection(url, user, password);
+		} catch(SQLException ex) {
+			
+			System.err.println(ex.getMessage());
+			
+		}
+	}
 	public void disconnect () {
-		
 	}
 	
-	public String sendQuerry(String sQuerry) {
-		return "";
+	public String[] sendQuerry(String sQuerry) throws SQLException {
+		String query = sQuerry;
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		stmt.close();
+		int columns = rs.getMetaData().getColumnCount();
+		String[] rsString = new String[columns];
+		while(rs.next()) {
+			for(int i=1;i<=columns;i++) {
+				rsString[i] = rs.getString(i);
+			}
+		}
+		return rsString;
 	}
 	
+	public void sendQuerryNoReturn(String sQuerry) throws SQLException {
+		String query = sQuerry;
+		Statement stmt = conn.createStatement();
+		stmt.execute(query);
+		stmt.close();
+	}
 }
